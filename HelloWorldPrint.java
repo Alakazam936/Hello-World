d11621bee366d2e950c11e975842d1ed1558e398
Alex <AlexDeSilvio@outlook.com>
2025-03-02 15:33:10 -0500

    First commit

---
+++ HelloWorldPrint.java
@@ -0,0 +1,15 @@
+public class HelloWorldPrint
+{
+    public static void Print()
+    {
+        System.out.println("Hello World - 2 !!");
+    
+    }
+    
+    public static void main(String[] args) {
+        
+        Print();
+    
+    }   
+}
+
